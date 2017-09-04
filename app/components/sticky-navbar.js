0c4a01f8e0efe6df5e4ee65359757ab7f92bfc81
Kevin Fallas <kev.fallas@gmail.com>
2017-09-04 14:31:51 -0600

    Logo resizing on scroll implemented

---
+++ app/components/sticky-navbar.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+const {
+    inject,
+    Component
+} = Ember;
+
+
+export default Ember.Component.extend({
+    windowscroll: inject.service(),
+    didInsertElement: function () {
+        this.get('windowscroll').on('scroll', (e) => {
+           var Logo = document.getElementById("logo").classList;
+           
+            if ($(window).scrollTop() > 30) {
+                Logo.remove('company-logo');
+                Logo.add("company-logo-small");                
+            } else {
+                Logo.remove('company-logo-small');
+                Logo.add("company-logo");                 
+            }
+        });
+    }
+
+});
---
+++ app/controllers/index.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    myStickyOptions: {
+        topSpacing: 30, //px, default: 0 
+        zIndex: 9999
+    },
+});
